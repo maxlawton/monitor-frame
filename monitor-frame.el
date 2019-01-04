393254d89f2ed11c016c6904ddfcad3a29ef534b
Max Lawton <max@mauldineconomics.com>
2019-01-04 16:48:07 -0500

    Implement monitor-frame-on* and fullscreen functions
    Uses display-monitor-attributes-alist to get list of monitors and the
    associated frames.
    
    The monitor-frame-fullscreen-all iterates over all frames and makes
    them fullscreen.

--- monitor-frame.el
+++ monitor-frame.el
@@ -1,27 +1,37 @@
 ;; Monitor frame commands
 
-(defun monitor-frame-position (monitor)
-    (if (eq monitor 'left)
-        '((top . 4) (left + -2044))
-        '((top . 4) (left . 2052))))
+(require 'frame)
 
-(defun monitor-frame-create (monitor)
-    "Create a monitor on the specified display"
-    (interactive "S")
-    (let ((primary (selected-frame))
-             (secondary (make-frame)))
-        (progn
-            (monitor-frame-move secondary monitor)
-            (select-frame primary))))
+(defun monitor-frame-on-monitor (monitor)
+  "Make a frame on monitor"
+  (let* ((geometry (cdr (assq 'geometry monitor)))
+          (left (car geometry))
+          (top (cadr geometry))
+          frame)
 
-(defun monitor-frame-move (frame monitor)
-    (select-frame frame)
-    (toggle-frame-fullscreen)
-    (modify-frame-parameters frame (monitor-frame-position monitor))
-    (toggle-frame-fullscreen))
+    (setq frame (make-frame `((top . ,top) (left . ,left) (fullscreen . nil))))
+    (set-frame-parameter frame 'fullscreen 'fullboth)))
 
-;; (monitor-frame-move (other-frame 1) 'right)
+(defun monitor-frame-on-all ()
+  "Make frames on all monitors"
+  (interactive)
+  (let ((lengths '()))
+    (dolist (monitor (display-monitor-attributes-list))
+      (let ((frames (length (cdr (assq 'frames monitor)))))
+        (when (eq 0 frames) (monitor-frame-on-monitor monitor))))))
 
+(defun monitor-frame-fullscreen-all ()
+  "Make all of the frames fullscreen"
+  (interactive)
+  (dolist (frame (visible-frame-list))
+    (set-frame-parameter frame 'fullscreen 'fullboth)))
 
+(defun monitor-frame-fullscreen-none ()
+  "Make none of the frames fullscreen"
+  (interactive)
+  (dolist (frame (visible-frame-list))
+    (set-frame-parameter frame 'fullscreen nil)))
+
+(define-key ctl-x-5-map "a" 'monitor-frame-on-all)
 
 (provide 'monitor-frame)
